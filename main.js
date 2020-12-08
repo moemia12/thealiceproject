84e3a57d51c35d9a6ff6cb1300494e55e476ac4b
Mohsin Miah <moemia12@gmail.com>
2020-12-08 04:26:22 +0000

    Test

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 //Male Female Buttons
 
 let weight = document.getElementsByClassName("weight-input");
+let amount = document.getElementsByClassName("alcohol-content")
+let units = document.getElementsByClassName("measurement-units")
 
+let alcohol = 
+
+
+let wine = alcohol 
+let beer = alcohol 
+
+
+let amount = 4;
+let weight = 4;
+
+const submit = ()=>{
+    amount*units
+}
+
+
+const bloodAlcohol = () =>{
+
+    weight = 50
+    numberOfDrinks = 10
+    typeOfDrinks = spirit
+    
+
+}
