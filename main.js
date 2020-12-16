3aff1faa49a115c98e68ef0c72eec1339b890bb0
Mohsin Miah <moemia12@gmail.com>
2020-12-16 18:44:55 +0000

    New Research

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
 //Male Female Buttons
 
+// 1% of alocohol is 0.35g of alcohol
+// To translate volume (e.g. ml) to mass (e.g. g) and vice versa, it is necessary to know the density. Alcohol density d=0.8g/ml. 
+// Thus: a wine at 12.5 % vol contains 12.5ml of alcohol/100ml of wine x 0.8 g/ml = 10g of alcohol/100 ml of wine. This is the equivalent of 1 drinking unit (= 10 g).
+
 let weight = document.getElementsByClassName("weight-input");
 let amount = document.getElementsByClassName("alcohol-content")
 let units = document.getElementsByClassName("measurement-units")
+let submitButton = document.getElementById("submit-button")
 
-let alcohol = 
-
-
-let wine = alcohol 
-let beer = alcohol 
-
-
-let amount = 4;
-let weight = 4;
-
-const submit = ()=>{
-    amount*units
-}
-
-
-const bloodAlcohol = () =>{
-
-    weight = 50
-    numberOfDrinks = 10
-    typeOfDrinks = spirit
+submitButton.addEventListener('click', function(){
     
-
-}
+})
